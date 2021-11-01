1dc91f160f2e0baefa967a28c9f2b188bf9c2c58
KonyTech <nicolas.cortella@protonmail.com>
2021-11-01 14:45:10 +0100

    Removed unused gasReporter and etherscan from hardhat config 2/2

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -27,13 +27,6 @@ const config: HardhatUserConfig = {
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
   },
-  gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
-    currency: "USD",
-  },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
-  },
   paths: {
     root: "./hardhat/"
   },
